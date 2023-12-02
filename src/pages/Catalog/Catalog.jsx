3ac50e904b365c6def5fbcb8bb94c136a4d02973
strictly-rigid <holysociology@gmail.com>
2023-12-02 23:06:37 +0200

    add Filter by make functionality

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
 import { fetchCars } from "../../redux/operations";
 
 import {
   selectCars,
   selectIsLoading,
-  selectCurrentPage,
+  // selectCurrentPage,
 } from "../../redux/selectors";
 import CarItem from "../../components/CarITem/CarItem";
-import Dropdown from "../../components/Dropdown/Dropdown";
+// import Dropdown from "../../components/Dropdown/Dropdown";
+import carMakes from "../../sources/makes.json";
 import css from "./Catalog.module.css";
+import { nanoid } from "nanoid";
 
 export default function Catalog() {
   const dispatch = useDispatch();
   const allCars = useSelector(selectCars);
   const isLoading = useSelector(selectIsLoading);
+  const [selectOption, setSelectOption] = useState(null);
   // let currentPage = useSelector(selectCurrentPage);
 
   useEffect(() => {
@@ -27,16 +28,36 @@ export default function Catalog() {
   //   currentPage += 1;
   // };
 
+  const filteredCars = selectOption
+    ? allCars.filter((carItem) => carItem.make === selectOption)
+    : allCars;
+
+  const handleDropdownChange = (event) => {
+    setSelectOption(event.target.value);
+  };
+  console.log(filteredCars);
+
   return (
     <div>
       <div className={css.dropdownContainer}>
-        <Dropdown></Dropdown>
+        <label htmlFor="dropdown">Car brand</label>
+        <select
+          id="dropdown"
+          name="dropdown"
+          value={selectOption}
+          onChange={handleDropdownChange}>
+          {carMakes.map((make) => (
+            <option key={nanoid()} value={make}>
+              {make}
+            </option>
+          ))}
+        </select>
       </div>
       <div className={css.catalogContainer}>
         {isLoading ? (
           <p>Loading...</p>
         ) : (
-          allCars.map((carItem) => (
+          filteredCars.map((carItem) => (
             <CarItem
               className={css.catalogItem}
               key={carItem.id}
