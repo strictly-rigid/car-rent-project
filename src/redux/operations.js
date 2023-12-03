d0613a5d97d358e7ea7e163860577b8e6e03d86c
strictly-rigid <holysociology@gmail.com>
2023-12-03 18:02:27 +0200

    update readme

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { initialState } from "./carsSlice";
 
 axios.defaults.baseURL = "https://64e8d9f999cf45b15fe03b41.mockapi.io";
 
+const page = 1;
+const limit = 12;
+
 export const fetchCars = createAsyncThunk(
   "cars/fetchCars",
-  async (_, thunkAPI) => {
+  async (params, thunkAPI) => {
     try {
-      // const { currentPage, perPage } = initialState;
-      const response = await axios.get(`/advert`);
+      const response = await axios.get(`/advert`, {
+        params: {
+          page: page,
+          limit: limit,
+        },
+      });
       return response.data;
     } catch (error) {
       console.error(error);
